e4848490440c85f6b594c60b370c29f5ffd6c2e0
Lindsay Gaff <lindsaygaff@gmail.com>
2015-04-16 18:19:10 +1000

    Fix io inlining

--- src/include/sys/io.h
+++ src/include/sys/io.h
@@ -1,11 +1,31 @@
-#ifndef _IO_H
-#define _IO_H
+#include <stdint.h>
 
-  static inline void outb (uint16_t port, uint8_t val);
-  static inline void outw (uint16_t port, uint16_t val);
-  static inline void outl (uint16_t port, uint32_t val);
+static inline void outb (uint16_t port, uint8_t val) {
+  asm volatile ("outb %0, %1" : : "a"(val), "Nd"(port));
+}
 
-  static inline uint8_t inb (uint16_t port);
-  static inline uint16_t inw (uint16_t port);
-  static inline uint32_t inl (uint16_t port);
-#endif
+static inline void outw (uint16_t port, uint16_t val) {
+  asm volatile ("outw %0, %1" : : "a"(val), "Nd"(port));
+}
+
+static inline void outl (uint16_t port, uint32_t val) {
+  asm volatile ("outl %0, %1" : : "a"(val), "Nd"(port));
+}
+
+static inline uint8_t inb (uint16_t port) {
+  uint8_t ret;
+  asm volatile ("inb %1, %0" : "=a"(ret) : "Nd"(port));
+  return ret;
+}
+
+static inline uint16_t inw (uint16_t port) {
+  uint16_t ret;
+  asm volatile ("inw %1, %0" : "=a"(ret) : "Nd"(port));
+  return ret;
+}
+
+static inline uint32_t inl (uint16_t port) {
+  uint32_t ret;
+  asm volatile ("inl %1, %0" : "=a"(ret) : "Nd"(port));
+  return ret;
+}
