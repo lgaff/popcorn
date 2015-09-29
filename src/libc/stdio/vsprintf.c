cd005b69fb577ccbe84ad6ea482966c85e85cd5c
Lindsay Gaff <lindsaygaff@gmail.com>
2015-09-29 22:52:01 +1000

    left alignment implemented for string, char types

--- src/libc/stdio/vsprintf.c
+++ src/libc/stdio/vsprintf.c
@@ -6,21 +6,19 @@
 typedef struct flags {
   bool pad_zeroes;
   bool min_width;
+  bool left_align;
 } flags_t;
-
-int pad_field(char *stream, const char padchar, int size) {
-  char *sp = stream;
-  for (int i = 0; i < size; i++) {
-    *sp++ = padchar;
-  }
-  return sp - stream;
-}
 
 int vsprintf(char *stream, const char *format, va_list args) {
   char *sp = stream;
   char * s;
-  flags_t attributes = { .pad_zeroes = false, .min_width = false };
-  int field_width = 0;
+  int field_width = -1;
+  int len;
+  flags_t attributes = { .pad_zeroes = false,
+                         .min_width = false,
+                         .left_align = false
+  };
+
   for (const char *fp = format; *fp; fp++) {
     if (*fp != '%') {
       *sp++ = *fp;
@@ -33,13 +31,16 @@ int vsprintf(char *stream, const char *format, va_list args) {
       if (*fp == '0') {
         attributes.pad_zeroes = true;
       }
+      else if (*fp == '-')
+        attributes.left_align = true;        
       else break;
       fp++;
     }
-
-    // Field width
+    
+    // Field width Skip a sign if given.
+    if (*fp == '-' || *fp == '+')
+      fp++;
     if (*fp >= '1' && *fp <= '9') {
-      attributes.min_width = true;
       field_width += *fp++ - '0';
       while (*fp >= '0' && *fp <= '9' && *fp) {
         field_width *= 10;
@@ -48,22 +49,24 @@ int vsprintf(char *stream, const char *format, va_list args) {
       }
     }
     
+    
     switch (*fp) {
     case '%': *sp++ = '%'; break;
     case 'c':
-      if (attributes.min_width) {
-        sp += pad_field(sp, ' ', field_width - 1);
-      }
-        *sp++ = (char)va_arg(args, int); break;
+      if (!attributes.left_align)
+        while (field_width-- > 0) *sp++ = ' ';
+      *sp++ = (char)va_arg(args, int);
+      if (attributes.left_align)
+        while (field_width-- > 0) *sp++ = ' ';
+      break;
     case 's':
-      s = (char *)va_arg(args, char *);
-      if (attributes.min_width) {
-        if (field_width > (int)strlen (s))
-          sp += pad_field (sp, ' ', field_width - strlen(s));
-      }
-      while(*s) {
-        *sp++ = *s++;
-      }
+      s = va_arg(args, char *);
+      len = strlen(s);
+      if (!attributes.left_align)
+        while (len <= field_width--) *sp++ = ' ';
+      while(*s) *sp++ = *s++;
+      if (attributes.left_align)
+        while (len <= field_width--) *sp++ = ' ';
       break;
     default: // unrecognised flag.
       return -1;
