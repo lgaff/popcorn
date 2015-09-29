989316d0654e8626b1c09c047134ec32f2b6a478
Lindsay Gaff <lindsaygaff@gmail.com>
2015-09-29 19:23:38 +1000

    Field width specifier for single-character types, pad zero flag

--- src/libc/stdio/vsprintf.c
+++ src/libc/stdio/vsprintf.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
+#include <limits.h>
+#include <stdbool.h>
 
 typedef struct flags {
-  char alternate;
-  char pad_zeroes;
-  char left_align;
-  char add_blank;
-  char add_sign;
-  char precision_set;
+  bool pad_zeroes;
+  bool min_width;
 } flags_t;
 
 int pad_field(char *stream, const char padchar, int size) {
@@ -14,21 +12,48 @@ int pad_field(char *stream, const char padchar, int size) {
   for (int i = 0; i < size; i++) {
     *sp++ = padchar;
   }
-  return *sp - *stream;
+  return sp - stream;
 }
 
 int vsprintf(char *stream, const char *format, va_list args) {
   char *sp = stream;
   char * s;
+  flags_t attributes = { .pad_zeroes = false, .min_width = false };
+  int field_width = 0;
   for (const char *fp = format; *fp; fp++) {
     if (*fp != '%') {
       *sp++ = *fp;
       continue;
     }
     ++fp;
+
+    // Check attributes. Yes, an arbitrary number of flags.. There is probably a better way than this.
+    while (*fp) {
+      if (*fp == '0') {
+        attributes.pad_zeroes = true;
+      }
+      else break;
+      fp++;
+    }
+
+    // Field width
+    if (*fp >= '1' && *fp <= '9') {
+      attributes.min_width = true;
+      field_width += *fp++ - '0';
+      while (*fp >= '0' && *fp <= '9' && *fp) {
+        field_width *= 10;
+        field_width += *fp - '0';
+        fp++;
+      }
+    }
+    
     switch (*fp) {
     case '%': *sp++ = '%'; break;
-    case 'c': *sp++ = (char)va_arg(args, int); break;
+    case 'c':
+      if (attributes.min_width) {
+        sp += pad_field(sp, ' ', field_width - 1);
+      }
+        *sp++ = (char)va_arg(args, int); break;
     case 's':
       s = (char *)va_arg(args, char *);
       while(*s) {
