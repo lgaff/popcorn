43b22c51958232cef5701775d1180fbebae0b2ac
Lindsay Gaff <lindsaygaff@gmail.com>
2015-09-30 10:41:57 +1000

    print_number implemented for arbitrary-base integer to ascii conversion

--- src/libc/stdio/vsprintf.c
+++ src/libc/stdio/vsprintf.c
@@ -1,25 +1,76 @@
+/* WARNING WARNING WARNING WARNING
+   This code is currently riddled with potential buffer overruns and prone to abuse. tread carefully.
+   If you see somewhere that looks like it might cause nasties, it probably will, and is already known...
+*/
+
 #include <stdio.h>
 #include <limits.h>
-#include <stdbool.h>
+#include <stdint.h>
 #include <string.h>
 
-typedef struct flags {
-  bool pad_zeroes;
-  bool min_width;
-  bool left_align;
-  bool blank;
-} flags_t;
+#define PAD_ZERO   1
+#define LEFT_ALIGN 2
+#define BLANK      4
+
+char * print_number (char *stream, int n, int base, int field_width, uint8_t attributes) {
+  const char *digits = "0123456789abcdefghijklmnopqrstuvwxyz";
+  //char ** digits = &digits_lower;
+  int n_len = 0;
+  int n_tmp;
+  int digit;
+  char sign = 0;
+  char *sp = stream;
+
+  if (n < 0) {
+    sign = '-';
+    n = -n;
+  }
+  else if (attributes & BLANK) sign = ' ';
+
+  n_tmp = n;
+  if (n == 0) n_len = 1;
+  while (n_tmp > 0) {
+    n_len++;
+    n_tmp = (n_tmp - n_tmp % base) / base;
+  }
+
+  if (sign) {
+    if (attributes & LEFT_ALIGN || attributes & PAD_ZERO) *sp++ = sign;
+    if (!(attributes & LEFT_ALIGN)) {
+      while (n_len < field_width--) *sp++ = (attributes & PAD_ZERO ? '0' : ' ');
+      if (!(attributes & PAD_ZERO)) *sp++ = sign;
+    }
+  }
+
+  else {
+    if (!(attributes & LEFT_ALIGN))
+      while (n_len <= field_width--) *sp++ = (attributes & PAD_ZERO ? '0' : ' ');
+  }
+
+  n_tmp = n;
+  for (int i = n_len-1; i >= 0; i--) {
+    digit = n_tmp % base;
+    *(sp + i) = digits[digit];
+    n_tmp = (n_tmp - digit) / base;
+  }
+
+  sp += n_len;
+
+  if (attributes & LEFT_ALIGN) {
+    while (n_len <= field_width - (sign ? 1 : 0)) {
+      *sp++ = ' ';
+      field_width--;
+    }
+  }
+  return sp;
+}
 
 int vsprintf(char *stream, const char *format, va_list args) {
   char *sp = stream;
   char * s;
   int field_width = -1;
   int len;
-  flags_t attributes = { .pad_zeroes = false, 
-                         .min_width  = false,
-                         .left_align = false,
-                         .blank      = false
-  };
+  uint8_t attributes = 0;
 
   for (const char *fp = format; *fp; fp++) {
     if (*fp != '%') {
@@ -30,9 +81,12 @@ int vsprintf(char *stream, const char *format, va_list args) {
   repeat_attribute:
     ++fp;
     switch (*fp) {
-    case '0': attributes.pad_zeroes = true; goto repeat_attribute;
-    case '-': attributes.left_align = true; goto repeat_attribute;
-    case ' ': attributes.blank      = true; goto repeat_attribute;
+    case '0': attributes |= PAD_ZERO;   goto repeat_attribute;
+    case '-':
+      attributes |= LEFT_ALIGN;
+      attributes |= !PAD_ZERO;
+      goto repeat_attribute;
+    case ' ': attributes |= BLANK;      goto repeat_attribute;
     }
     
     // Field width Skip a sign if given.
@@ -51,25 +105,27 @@ int vsprintf(char *stream, const char *format, va_list args) {
     switch (*fp) {
     case '%': *sp++ = '%'; break;
     case 'c':
-      if (!attributes.left_align)
+      if (!(attributes & LEFT_ALIGN))
         while (field_width-- > 0) *sp++ = ' ';
       *sp++ = (char)va_arg(args, int);
-      if (attributes.left_align)
+      if (attributes & LEFT_ALIGN)
         while (field_width-- > 0) *sp++ = ' ';
       break;
     case 's':
       s = va_arg(args, char *);
       len = strlen(s);
-      if (!attributes.left_align)
+      if (!(attributes & LEFT_ALIGN))
         while (len <= field_width--) *sp++ = ' ';
-      if (attributes.blank && len == 0) {
+      if ((attributes & BLANK) && len == 0) {
         *sp++ = ' ';
         field_width--;
       }
       while(*s) *sp++ = *s++;
-      if (attributes.left_align)
+      if (attributes  & LEFT_ALIGN)
         while (len <= field_width--) *sp++ = ' ';
       break;
+    case 'd':
+      sp = print_number (sp, va_arg(args, int), 10, field_width, attributes); break;
     default: // unrecognised flag.
       return -1;
     }      
