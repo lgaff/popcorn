7982ce80b3d3123617d923793eebdf18b92eb0ee
Lindsay Gaff <lindsaygaff@gmail.com>
2015-10-06 18:13:48 +1100

    ANSI escape sequences
    This is a partial implementation of the ANSI console specification for Popcorn

---
+++ src/popcorn/include/ansi.h
@@ -0,0 +1,34 @@
+#ifndef _ANSI_H
+#define _ANSI_H
+
+#define ANSI_ESC 0x1B
+#define ANSI_CSI '['
+
+#define ANSI_CUU 'A'
+#define ANSI_CUD 'B'
+#define ANSI_CUF 'C'
+#define ANSI_CUB 'D'
+
+#define ANSI_CNL 'E'
+#define ANSI_CPL 'F'
+#define ANSI_CHA 'G'
+
+#define ANSI_CUP 'H'
+
+#define ANSI_ED 'J'
+#define ANSI_EL 'K'
+
+#define ANSI_SU 'S'
+#define ANSI_SD 'T'
+
+#define ANSI_HVP 'f'
+#define ANSI_SGR 'm'
+
+#define ANSI_AUX 'i'
+
+#define ANSI_SCP 's'
+#define ANSI_RCP 'u'
+
+
+
+#endif /* _ANSI_H */
